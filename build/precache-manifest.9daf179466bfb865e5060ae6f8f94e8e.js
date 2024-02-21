0df9a4e68a8875b44dc553abeac1436272572703
VJ <vishnujadhav5501@hmail.com>
2024-02-21 15:23:34 +0530

    add

---
+++ build/precache-manifest.9daf179466bfb865e5060ae6f8f94e8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7b81ab4564ab60cae36f683afe079e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aacceeb98f42cd042d0f",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "3c43944926ff6f4a3bb5",
+    "url": "/static/css/main.4289a7c4.chunk.css"
+  },
+  {
+    "revision": "aacceeb98f42cd042d0f",
+    "url": "/static/js/2.4d8fb14b.chunk.js"
+  },
+  {
+    "revision": "3c43944926ff6f4a3bb5",
+    "url": "/static/js/main.b3941c09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
